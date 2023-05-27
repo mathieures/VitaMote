cfba69b1df99923baf981ca177ca668a7405da68
mathieures <71087433+mathieures@users.noreply.github.com>
2023-05-28 01:33:36 +0200

    WIP: added single key presses to the input parser

---
+++ VitaMote/ButtonHelper.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Android.Views;
+
+namespace VitaMote
+{
+    class ButtonHelper
+    {
+
+        // Buttons
+        // Type 1
+        public const int btnL = 128;
+        public const int btnD = 64;
+        public const int btnR = 32;
+        public const int btnU = 16;
+        public const int btnSta = 8;
+        public const int btnSel = 1;
+        // Combos
+        // Dpad Combos
+        public const int btnLD = btnL + btnD;
+        public const int btnLU = btnL + btnU;
+        public const int btnDR = btnD + btnR;
+        public const int btnRU = btnR + btnU;
+        // Dpad + Sel or Sta
+        public const int btnUSt = btnU + btnSta;
+        public const int btnUSe = btnU + btnSel;
+        public const int btnDSt = btnD + btnSta;
+        public const int btnDSe = btnD + btnSel;
+        public const int btnLSt = btnL + btnSta;
+        public const int btnLSe = btnL + btnSel;
+        public const int btnRSt = btnR + btnSta;
+        public const int btnRSe = btnR + btnSel;
+        public const int btnStSe = btnSta + btnSel;
+        // Triple Combo (Dpad Combo + Sta)
+        public const int btnLUSt = btnLU + btnSta;
+        public const int btnLUSe = btnLU + btnSel;
+        public const int btnLDSt = btnLD + btnSta;
+        public const int btnLDSe = btnLD + btnSel;
+        public const int btnRUSt = btnRU + btnSta;
+        public const int btnRUSe = btnRU + btnSel;
+        public const int btnDRSt = btnDR + btnSta;
+        public const int btnDRSe = btnDR + btnSel;
+        // Type 2
+        public const int btnS = 128;
+        public const int btnX = 64;
+        public const int btnC = 32;
+        public const int btnT = 16;
+        public const int btnRt = 2;
+        public const int btnLt = 1;
+        // Double Combo
+        public const int btnXS = btnX + btnS;
+        public const int btnXC = btnX + btnC;
+        public const int btnCT = btnC + btnT;
+        public const int btnTS = btnT + btnS;
+        public const int btnXLt = btnX + btnLt;
+        public const int btnCLt = btnC + btnLt;
+        public const int btnTLt = btnT + btnLt;
+        public const int btnSLt = btnS + btnLt;
+        public const int btnXRt = btnX + btnRt;
+        public const int btnCRt = btnC + btnRt;
+        public const int btnTRt = btnT + btnRt;
+        public const int btnSRt = btnS + btnRt;
+        public const int btnLtRt = btnLt + btnRt;
+        // Triple Combo (L+R+SXCT)
+        public const int btnLtRtC = btnLt + btnRt + btnC;
+        public const int btnLtRtS = btnLt + btnRt + btnS;
+        public const int btnLtRtT = btnLt + btnRt + btnT;
+        public const int btnLtRtX = btnLt + btnRt + btnX;
+
+        // Input to keycode conversions
+
+        // DPAD
+        public const Keycode bUp = Keycode.DpadUp;
+        public const Keycode bDo = Keycode.DpadDown;
+        public const Keycode bLe = Keycode.DpadLeft;
+        public const Keycode bRi = Keycode.DpadRight;
+        // STA-SEL
+        public const Keycode bSt = Keycode.Back;
+        public const Keycode bSe = Keycode.DpadCenter;
+        // SXCT
+        public const Keycode bS = Keycode.ButtonX;
+        public const Keycode bX = Keycode.ButtonA;
+        public const Keycode bC = Keycode.ButtonB;
+        public const Keycode bT = Keycode.ButtonY;
+        // L-R Triggers
+        public const Keycode bLt = Keycode.ButtonL1;
+        public const Keycode bRt = Keycode.ButtonR1;
+        // Analog sticks
+        // LEFT
+        public const Keycode aLl = Keycode.A;
+        public const Keycode aLr = Keycode.D;
+        public const Keycode aLu = Keycode.W;
+        public const Keycode aLd = Keycode.S;
+        // RIGHT
+        public const Keycode aRl = Keycode.J;
+        public const Keycode aRr = Keycode.L;
+        public const Keycode aRu = Keycode.I;
+        public const Keycode aRd = Keycode.K;
+    }
+}
--- VitaMote/TestActivity.cs
+++ VitaMote/TestActivity.cs
@@ -1,12 +1,14 @@
 ﻿// Activity that will act as a network tester, to see if keys (or anything, really) are received from the PSVita
 
 using System;
+using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 using Android.App;
 using Android.OS;
 using Android.Util;
+using Android.Views;
 using Android.Widget;
 using Xamarin.Essentials;
 
@@ -90,6 +92,7 @@ namespace VitaMote
         {
             NetworkStream stream = tcpClient.GetStream();
             byte[] receivedBytes = new byte[8];
+            int[] receivedInts = new int[8]; // Keycodes cannot be compared to bytes
 
             while (true)
             {
@@ -100,7 +103,14 @@ namespace VitaMote
                     // Request the PSVita for new input
                     RequestInput(stream);
 
+                    // Read 8 bytes from the stream
                     stream.Read(receivedBytes, 0, 8);
+
+                    // Convert them to ints
+                    for (int i = 0; i < 8; i++)
+                        receivedInts[i] = (int)receivedBytes[i];
+
+                    ParseInts(receivedInts);
 
                     string convertedBytes = "";
                     foreach (var b in receivedBytes)
@@ -117,6 +127,318 @@ namespace VitaMote
                     break;
                 }
             }
+        }
+
+        void ParseInts(int[] ints)
+        {
+            // Maps all keys to a boolean representing the state of the key:
+            // if true, the key is Down (pressed), else it's Up (released)
+            var dictionary = new Dictionary<Keycode, bool>
+            {
+                // DPAD
+                { Keycode.DpadLeft, false },
+                { Keycode.DpadDown, false },
+                { Keycode.DpadRight, false },
+                { Keycode.DpadUp, false },
+                // Start, Select
+                { Keycode.Back, false },
+                { Keycode.DpadCenter, false },
+                // SXCT
+                { Keycode.ButtonX, false },
+                { Keycode.ButtonA, false },
+                { Keycode.ButtonB, false },
+                { Keycode.ButtonY, false },
+                // L-R Triggers
+                { Keycode.ButtonR1, false },
+                { Keycode.ButtonL1, false },
+                // Analog sticks
+                // LEFT
+                { Keycode.W, false },
+                { Keycode.S, false },
+                { Keycode.A, false },
+                { Keycode.D, false },
+                // RIGHT
+                { Keycode.I, false },
+                { Keycode.K, false },
+                { Keycode.J, false },
+                { Keycode.L, false }
+            };
+
+            // First int takes care of DPAD, Start, Select
+            switch (ints[0])
+            {
+                case ButtonHelper.btnL:
+                    dictionary[ButtonHelper.bLe] = true;
+                    break;
+                case ButtonHelper.btnD:
+                    dictionary[ButtonHelper.bDo] = true;
+                    break;
+                case ButtonHelper.btnR:
+                    dictionary[ButtonHelper.bRi] = true;
+                    break;
+                case ButtonHelper.btnU:
+                    dictionary[ButtonHelper.bUp] = true;
+                    break;
+                case ButtonHelper.btnSta:
+                    dictionary[ButtonHelper.bSt] = true;
+                    break;
+                case ButtonHelper.btnSel:
+                    dictionary[ButtonHelper.bSe] = true;
+                    break;
+                // Double combos (DPAD, Start, Select)
+                // Left + Up
+                case ButtonHelper.btnLU:
+                    dictionary[ButtonHelper.bLe] = true;
+                    dictionary[ButtonHelper.bUp] = true;
+                    break;
+                // Left + Down
+                case ButtonHelper.btnLD:
+                    dictionary[ButtonHelper.bLe] = true;
+                    dictionary[ButtonHelper.bDo] = true;
+                    break;
+                // Right + Up
+                case ButtonHelper.btnRU:
+                    dictionary[ButtonHelper.bRi] = true;
+                    dictionary[ButtonHelper.bUp] = true;
+                    break;
+                // Down + Right
+                case ButtonHelper.btnDR:
+                    dictionary[ButtonHelper.bDo] = true;
+                    dictionary[ButtonHelper.bRi] = true;
+                    break;
+                // Up + Start
+                case ButtonHelper.btnUSt:
+                    dictionary[ButtonHelper.bUp] = true;
+                    dictionary[ButtonHelper.bSt] = true;
+                    break;
+                // Up + Select
+                case ButtonHelper.btnUSe:
+                    dictionary[ButtonHelper.bSe] = true;
+                    dictionary[ButtonHelper.bUp] = true;
+                    break;
+                // Down + Start
+                case ButtonHelper.btnDSt:
+                    dictionary[ButtonHelper.bDo] = true;
+                    dictionary[ButtonHelper.bSt] = true;
+                    break;
+                // Down + Select
+                case ButtonHelper.btnDSe:
+                    dictionary[ButtonHelper.bDo] = true;
+                    dictionary[ButtonHelper.bSe] = true;
+                    break;
+                // Left + Start
+                case ButtonHelper.btnLSt:
+                    dictionary[ButtonHelper.bLe] = true;
+                    dictionary[ButtonHelper.bSt] = true;
+                    break;
+                // Left + Select
+                case ButtonHelper.btnLSe:
+                    dictionary[ButtonHelper.bLe] = true;
+                    dictionary[ButtonHelper.bSe] = true;
+                    break;
+                // Right + Start
+                case ButtonHelper.btnRSt:
+                    dictionary[ButtonHelper.bRi] = true;
+                    dictionary[ButtonHelper.bSt] = true;
+                    break;
+                // Right + Select
+                case ButtonHelper.btnRSe:
+                    dictionary[ButtonHelper.bRi] = true;
+                    dictionary[ButtonHelper.bSe] = true;
+                    break;
+                // Start + Select
+                case ButtonHelper.btnStSe:
+                    dictionary[ButtonHelper.bSt] = true;
+                    dictionary[ButtonHelper.bSe] = true;
+                    break;
+                // Triple combos (DPAD, Start, Select)
+                // Left + Up + Start
+                case ButtonHelper.btnLUSt:
+                    dictionary[ButtonHelper.bLe] = true;
+                    dictionary[ButtonHelper.bUp] = true;
+                    dictionary[ButtonHelper.bSt] = true;
+                        break;
+                // Left + Up + Select
+                case ButtonHelper.btnLUSe:
+                    dictionary[ButtonHelper.bLe] = true;
+                    dictionary[ButtonHelper.bUp] = true;
+                    dictionary[ButtonHelper.bSe] = true;
+                    break;
+                // Left + Down + Start
+                case ButtonHelper.btnLDSt:
+                    dictionary[ButtonHelper.bLe] = true;
+                    dictionary[ButtonHelper.bDo] = true;
+                    dictionary[ButtonHelper.bSt] = true;
+                    break;
+                // Left + Down + Select
+                case ButtonHelper.btnLDSe:
+                    dictionary[ButtonHelper.bLe] = true;
+                    dictionary[ButtonHelper.bDo] = true;
+                    dictionary[ButtonHelper.bSe] = true;
+                    break;
+                // Right + Up + Start
+                case ButtonHelper.btnRUSt:
+                    dictionary[ButtonHelper.bRi] = true;
+                    dictionary[ButtonHelper.bUp] = true;
+                    dictionary[ButtonHelper.bSt] = true;
+                    break;
+                // Right + Up + Select
+                case ButtonHelper.btnRUSe:
+                    dictionary[ButtonHelper.bRi] = true;
+                    dictionary[ButtonHelper.bUp] = true;
+                    dictionary[ButtonHelper.bSe] = true;
+                    break;
+                // Right + Down + Start
+                case ButtonHelper.btnDRSt:
+                    dictionary[ButtonHelper.bDo] = true;
+                    dictionary[ButtonHelper.bRi] = true;
+                    dictionary[ButtonHelper.bSt] = true;
+                    break;
+                // Right + Down + Select
+                case ButtonHelper.btnDRSe:
+                    dictionary[ButtonHelper.bDo] = true;
+                    dictionary[ButtonHelper.bRi] = true;
+                    dictionary[ButtonHelper.bSe] = true;
+                    break;
+                default:
+                    throw new Exception(
+                        "If this happens, it means not all the codes are listed in" +
+                        "ButtonHelper (or there was an error in the received packet)"
+                    );
+            }
+
+            // Second int takes care of SXCT, L trigger, R trigger
+            switch (ints[1])
+            {
+                case ButtonHelper.btnS:
+                    dictionary[ButtonHelper.bS] = true;
+                    break;
+                case ButtonHelper.btnX:
+                    dictionary[ButtonHelper.bX] = true;
+                    break;
+                case ButtonHelper.btnC:
+                    dictionary[ButtonHelper.bC] = true;
+                    break;
+                case ButtonHelper.btnT:
+                    dictionary[ButtonHelper.bT] = true;
+                    break;
+                case ButtonHelper.btnRt:
+                    dictionary[ButtonHelper.bLt] = true;
+                    break;
+                case ButtonHelper.btnLt:
+                    dictionary[ButtonHelper.bRt] = true;
+                    break;
+                // Double combos (SXCT, L, R)
+                case ButtonHelper.btnXC:
+                    dictionary[ButtonHelper.bX] = true;
+                    dictionary[ButtonHelper.bC] = true;
+                    break;
+                case ButtonHelper.btnXS:
+                    dictionary[ButtonHelper.bX] = true;
+                    dictionary[ButtonHelper.bS] = true;
+                    break;
+                case ButtonHelper.btnCT:
+                    dictionary[ButtonHelper.bC] = true;
+                    dictionary[ButtonHelper.bT] = true;
+                    break;
+                case ButtonHelper.btnTS:
+                    dictionary[ButtonHelper.bT] = true;
+                    dictionary[ButtonHelper.bS] = true;
+                    break;
+                case ButtonHelper.btnXLt:
+                    dictionary[ButtonHelper.bX] = true;
+                    dictionary[ButtonHelper.bLt] = true;
+                    break;
+                case ButtonHelper.btnXRt:
+                    dictionary[ButtonHelper.bX] = true;
+                    dictionary[ButtonHelper.bRt] = true;
+                    break;
+                case ButtonHelper.btnCLt:
+                    dictionary[ButtonHelper.bC] = true;
+                    dictionary[ButtonHelper.bLt] = true;
+                    break;
+                case ButtonHelper.btnCRt:
+                    dictionary[ButtonHelper.bC] = true;
+                    dictionary[ButtonHelper.bRt] = true;
+                    break;
+                case ButtonHelper.btnTLt:
+                    dictionary[ButtonHelper.bT] = true;
+                    dictionary[ButtonHelper.bLt] = true;
+                    break;
+                case ButtonHelper.btnTRt:
+                    dictionary[ButtonHelper.bT] = true;
+                    dictionary[ButtonHelper.bRt] = true;
+                    break;
+                case ButtonHelper.btnSLt:
+                    dictionary[ButtonHelper.bS] = true;
+                    dictionary[ButtonHelper.bLt] = true;
+                    break;
+                case ButtonHelper.btnSRt:
+                    dictionary[ButtonHelper.bS] = true;
+                    dictionary[ButtonHelper.bRt] = true;
+                    break;
+                case ButtonHelper.btnLtRt:
+                    dictionary[ButtonHelper.bLt] = true;
+                    dictionary[ButtonHelper.bRt] = true;
+                    break;
+                // Triple combos (SXCT, L, R)
+                case ButtonHelper.btnLtRtC:
+                    dictionary[ButtonHelper.bC] = true;
+                    dictionary[ButtonHelper.bLt] = true;
+                    dictionary[ButtonHelper.bRt] = true;
+                    break;
+                case ButtonHelper.btnLtRtT:
+                    dictionary[ButtonHelper.bT] = true;
+                    dictionary[ButtonHelper.bLt] = true;
+                    dictionary[ButtonHelper.bRt] = true;
+                    break;
+                case ButtonHelper.btnLtRtS:
+                    dictionary[ButtonHelper.bS] = true;
+                    dictionary[ButtonHelper.bLt] = true;
+                    dictionary[ButtonHelper.bRt] = true;
+                    break;
+                case ButtonHelper.btnLtRtX:
+                    dictionary[ButtonHelper.bX] = true;
+                    dictionary[ButtonHelper.bLt] = true;
+                    dictionary[ButtonHelper.bRt] = true;
+                    break;
+                default:
+                    throw new Exception(
+                        "If this happens, it means not all the codes are listed in" +
+                        "ButtonHelper (or there was an error in the received packet)"
+                    );
+            }
+
+            // Third int is not used
+            // Fourth int is not used
+
+            // The fifth int and half of the sixth take care of the left analog stick
+            if (0 < ints[4] && ints[4] <= 50)
+                dictionary[ButtonHelper.aLl] = true;
+
+            if (200 < ints[4] && ints[4] <= 255)
+                dictionary[ButtonHelper.aLr] = true;
+
+            if (0 < ints[5] && ints[5] <= 50)
+                dictionary[ButtonHelper.aLu] = true;
+
+            if (200 < ints[5] && ints[5] <= 255)
+                dictionary[ButtonHelper.aLd] = true;
+
+            // Half of the sixth int and the seventh take care of the right analog stick
+            if (0 < ints[6] && ints[6] <= 50)
+                dictionary[ButtonHelper.aRl] = true;
+
+            if (200 < ints[6] && ints[6] <= 255)
+                dictionary[ButtonHelper.aRr] = true;
+
+            if (0 < ints[6] && ints[7] <= 50)
+                dictionary[ButtonHelper.aRu] = true;
+
+            if (200 < ints[7] && ints[7] <= 255)
+                dictionary[ButtonHelper.aRd] = true;
+
+            // TODO: draw conclusions
         }
 
         private readonly byte[] _requestBytes = System.Text.Encoding.ASCII.GetBytes("request");
